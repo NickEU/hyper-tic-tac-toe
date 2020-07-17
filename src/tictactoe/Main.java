668302f39463ba8afa065f6925d1f893b6310df9
Nick <neverate@gmail.com>
2020-07-17 14:57:38 +0300

    +implemented phase3. introduced a basic menu loop  -can choose player types now  -sweet AI vs AI and human vs human battles are now possible  -can exit the menu

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -3,6 +3,6 @@ package tictactoe;
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        ui.start();
+        ui.startPhase3();
     }
 }
--- src/tictactoe/UserInterface.java
+++ src/tictactoe/UserInterface.java
@@ -8,9 +8,73 @@ import java.util.Scanner;
 public class UserInterface {
     private final Scanner sc = new Scanner(System.in);
     private TicTacToe game;
+    private PlayerType playerX;
+    private PlayerType playerY;
 
-    public void start() {
-        game = TicTacToe.startNewGame(PlayerType.HUMAN, PlayerType.EASY);
+    public void startPhase3() {
+        while (true) {
+            boolean rdyToLeave = mainMenuLoopUntilExit();
+            if (rdyToLeave) {
+                break;
+            }
+            startGame();
+        }
+    }
+
+    private boolean mainMenuLoopUntilExit() {
+        String mainLoopErrorMsg = "Bad parameters";
+        while (true) {
+            System.out.print("Input command: ");
+            String[] userInput = sc.nextLine().split(" ");
+            String cmd = userInput[0];
+            if ("exit".equals(cmd)) {
+                return true;
+            }
+
+            if (userInput.length != 3) {
+                System.out.println(mainLoopErrorMsg);
+                continue;
+            }
+
+            String argX = userInput[1];
+            String argY = userInput[2];
+
+            if (userArgParseFailed(argX) || userArgParseFailed(argY)) {
+                System.out.println(mainLoopErrorMsg);
+            } else {
+                game = TicTacToe.startNewGame(playerX, playerY);
+                return false;
+            }
+        }
+    }
+
+    private boolean userArgParseFailed(String player) {
+        boolean isFirstArg = playerX == null;
+        switch (player) {
+            case "user":
+                if (isFirstArg) {
+                    playerX = PlayerType.HUMAN;
+                } else {
+                    playerY = PlayerType.HUMAN;
+                }
+                break;
+            case "easy":
+                if (isFirstArg) {
+                    playerX = PlayerType.EASY;
+                } else {
+                    playerY = PlayerType.EASY;
+                }
+                break;
+            default:
+                playerX = null;
+                playerY = null;
+                return true;
+        }
+        return false;
+    }
+
+    public void startGame() {
+        //game = TicTacToe.startNewGame(PlayerType.HUMAN, PlayerType.EASY);
         renderGameBoard();
         while (game.isInProgress()) {
             if (game.nextTurnIsHuman()) {
